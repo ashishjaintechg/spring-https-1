8acd2a3943dbdda7610ef0b4f0e85e3ba38c6753
ASHISH JAIN <ashishjaintechg@gmail.com>
2021-05-10 23:52:05 +0530

    https spring boot

---
+++ src/test/java/com/techg/spring/https/api/SpringHttps1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techg.spring.https.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttps1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
